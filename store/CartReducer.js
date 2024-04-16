868cf2e18cb9eb35df02f1537a0706241687238e
Jossuc4 <larryjossuc4@gmail.com>
2024-04-16 09:07:34 +0300

    love u

--- store/CartReducer.js
+++ store/CartReducer.js
@@ -1,39 +1,89 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { configureStore, createSlice } from "@reduxjs/toolkit"
 
 
 const cartSlice = createSlice({
     name: "cart",
     initialState: {
-        products: [
-            {
-                libelle: String,
-                nbre: Number
-            }
-        ]
+        products: [],
+        totalPrice: 0,
+        cartSize: 0
     },
     reducers: {
-        add: (state, productName) => {
+        addToCart: (state, action) => {
+            // { type:cart/addToCart,payload:{libelle: Nike supra,prix:25$ }}
 
-            let actualState = state.products
-            const existedInCart = actualState.find(item => item.libelle === productName)
+            const { products, cartSize, totalPrice } = state
+
+            const existedInCart = products.find(item => item.libelle === action.payload.libelle)
 
             if (existedInCart) {
 
-                const indexState = actualState.indexOf(existedInCart)
-                actualState[indexState].nbre += 1
-                state.products = actualState
+                // const indexState = products.indexOf(existedInCart)
+
+                products.map(item => {
+
+                    const { libelle, nbre, total, prix } = item
+
+                    if (libelle === action.payload.libelle) {
+                        nbre += 1
+                        total = prix * nbre
+                    }
+
+                })
+
+                // products[indexState].nbre += 1
+
+                // products = actualState
 
             } else {
-                state.products = [...actualState, { libelle: state, nbre: 1 }]
+
+                const { libelle, prix } = action.payload
+
+                products.push(
+                    {
+                        libelle,
+                        total: prix,
+                        nbre: 1
+                    }
+                )
             }
+
+            cartSize = products.length
+            totalPrice = calcul(products)
 
         },
 
-        remove: (state, productName) => {
+        removeFromCart: (state, action) => {
+            // { type:cart/removeFromCart,payload:Nike supra }
 
-            let newState = state.products.find(item => item.libelle !== productName)
-            state.products = newState
+            const { products, cartSize, totalPrice } = state
+
+            products = products.find(item => item.libelle !== action.payload.libelle)
+
+            cartSize = products.length
+            totalPrice = calcul(products)
+
+            return state
 
         }
     }
 })
+
+export const { addToCart, removeFromCart } = cartSlice.actions
+
+export const cartStore = configureStore(
+    {
+        reducer: {
+            cart: cartSlice.reducer
+        }
+    }
+)
+
+
+function calcul(products){
+    let somme = 0
+    if(products.length!==0){
+        products.map(product => somme+= parseInt(product.total))
+    }
+    return somme
+}
