d14db2802501f788d67131f9279b979a82db0c8c
Jossuc4 <larryjossuc4@gmail.com>
2024-05-11 10:57:00 +0300

    image path

---
+++ imagePath.js
@@ -0,0 +1,27 @@
+export const imagePath =
+{
+    "chaussures": require('./public/images/chaussures.png'),
+    "vêtements": require('./public/images/vêtements.png'),
+    "cooks_standard": require('./public/images/cooks_standard.png'),
+    "cutter_&_buck": require('./public/images/cutter_&_buck.png'),
+    "divertissement": require('./public/images/divertissement.png'),
+    "dragon_touch_android_tablet": require('./public/images/dragon_touch_android_tablet.png'),
+    "electromenager": require('./public/images/electromenager.png'),
+    "electronique": require('./public/images/electronique.png'),
+    "hanes": require('./public/images/hanes.png'),
+    "Chedbanz_2023_edition_cards_picture_guessing": require('./public/images/hedbanz_2023_edition_cards_picture_guessing.png'),
+    "honestbaby": require('./public/images/honestbaby.png'),
+    "jouets": require('./public/images/jouets.png'),
+    "mattel_games_blokus_xl_strategy_board_game": require('./public/images/mattel_games_blokus_xl_strategy_board_game.png'),
+    "meubles": require('./public/images/meubles.png'),
+    "Cninja_af101_air": require('./public/images/ninja_af101_air.png'),
+    "nintendo_switch": require('./public/images/nintendo_switch.png'),
+    "playstation_5": require('./public/images/playstation_5.png'),
+    "realspace_peakwood_65": require('./public/images/realspace_peakwood_65.png'),
+    "rotary_cheese": require('./public/images/rotary_cheese.png'),
+    "samsung_galaxy_tab_s6_lite": require('./public/images/samsung_galaxy_tab_s6_lite.png'),
+    "samsung_galaxy_tab_s8_11_128gb_wifi_6e_android_tablet": require('./public/images/samsung_galaxy_tab_s8_11_128gb_wifi_6e_android_tablet.png'),
+    "sauder_nova_loft_59_w_l-shape_corner_desk": require('./public/images/sauder_nova_loft_59_w_l-shape_corner_desk.png'),
+    "steve_madden": require('./public/images/steve_madden.png'),
+    "the_drop": require('./public/images/the_drop.png')
+}
