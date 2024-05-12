9d28e624b1562ad8e6d3a3b7c7b041cdf0903d63
Jossuc4 <larryjossuc4@gmail.com>
2024-05-13 00:24:42 +0300

    cards & buttons

--- components/Buttons/ChooseButton.jsx
+++ components/Buttons/ChooseButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { TouchableOpacity, Text, View } from "react-native";
+import { TouchableOpacity, Text, View, StyleSheet } from "react-native";
 import FontAwesome5Icon from "react-native-vector-icons/FontAwesome5";
 
 export default function ChooseButton({ title, onPress }) {
