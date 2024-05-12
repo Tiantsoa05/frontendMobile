e4427f86cc2a1bb9fb097206d0b7c8afb397ac41
Jossuc4 <larryjossuc4@gmail.com>
2024-05-12 14:51:12 +0300

    config

--- config/config.js
+++ config/config.js
@@ -4,6 +4,6 @@ export const IP_ADDRESS = '192.168.88.231'
 
 export const model = axios.create(
     {
-        baseURL: "http://192.168.56.1:3000/api"
+        baseURL: "http://192.168.88.17:3000/api"
     }
 )
